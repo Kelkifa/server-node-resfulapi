7280607934f93127d919e652f5e49091d70fbe68
Pham Long Huan <huanvip455@gmail.com>
2021-09-08 21:56:47 +0700

    fix bug 2

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -10,16 +10,16 @@ class HomeController {
      *  Create a new collection in mongodb
      *  public
     */
-    async createCollection(req, res) {
-        try {
-            const response = await gameModel.find().sort({ createdAt: 'desc' });
-            console.log(response);
-            return res.json({ success: true, message: response });
-        } catch (err) {
-            console.log(err);
-            return res.json({ success: false, message: 'fail' });
-        }
-    }
+    // async createCollection(req, res) {
+    //     try {
+    //         const response = await gameModel.find().sort({ createdAt: 'desc' });
+    //         console.log(response);
+    //         return res.json({ success: true, message: response });
+    //     } catch (err) {
+    //         console.log(err);
+    //         return res.json({ success: false, message: 'fail' });
+    //     }
+    // }
 }
 
 module.exports = new HomeController;
--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -2,8 +2,8 @@ const homeRouter = require('./home');
 const gameRouter = require('./game');
 
 function router(app) {
-    app.use('/', homeRouter);
     app.use('/api/games', gameRouter);
+    app.use('/', homeRouter);
 }
 
 module.exports = router;
