241f192cc1f17a75d05305108ae20c48fe5497cf
Pham Long Huan <huanvip455@gmail.com>
2021-10-27 10:12:18 +0700

    make it better

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -5,21 +5,6 @@ class HomeController {
     async index(req, res) {
         return res.json({ success: true, message: 'successfully' });
     }
-
-    /** [GET] /home/createCollection 
-     *  Create a new collection in mongodb
-     *  public
-    */
-    // async createCollection(req, res) {
-    //     try {
-    //         const response = await gameModel.find().sort({ createdAt: 'desc' });
-    //         console.log(response);
-    //         return res.json({ success: true, message: response });
-    //     } catch (err) {
-    //         console.log(err);
-    //         return res.json({ success: false, message: 'fail' });
-    //     }
-    // }
 }
 
 module.exports = new HomeController;
